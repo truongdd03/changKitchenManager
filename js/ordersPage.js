b352c4a600054148d49a934993f808f51e1c87d6
Dong Truong <dong23102003@gmail.com>
2021-06-02 10:33:58 +0700

    Add clicked order style

--- js/ordersPage.js
+++ js/ordersPage.js
@@ -1,10 +1,39 @@
+var currentID = -1;
+
 function load() {
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 20; i++) {
+
         var div = document.createElement('div');
         div.className = "OrderWrapper";
-        div.innerHTML = div.innerHTML + '<p class="OrderInformation">#123212<\p>'
-        div.innerHTML = div.innerHTML + '<p class="OrderInformation">Truong Dinh Dong<\p>'
-        div.innerHTML = div.innerHTML + '<p class="OrderPrice">$123<\p>'
-        document.getElementById('Main').append(div)
+        div.id = String(i);
+
+        div.innerHTML = div.innerHTML + '<p class="OrderInformation">#123212<\p>';
+        div.innerHTML = div.innerHTML + '<p class="OrderInformation">Truong Dinh Dong<\p>';
+        div.innerHTML = div.innerHTML + '<p class="OrderPrice">$123<\p>';
+
+        div.onclick = (function(i) { 
+            return function() { 
+                clicked(i);
+            }
+        })(i);
+
+        document.getElementById("OrdersWrapper").append(div);
     }
+}
+
+function print(i) {
+    console.log(i);
+}
+
+function clicked(id) {
+    var div = document.createElement('div');
+    div.innerHTML = '<p> Clicked <\p>';
+
+    document.getElementById(id).style.backgroundColor = "rgb(245, 228, 200)";
+    if (currentID != -1) {
+        document.getElementById(currentID).style.backgroundColor = "white";
+    }
+
+    currentID = id;
+    document.getElementById("DishesWrapper").append(div);
 }
