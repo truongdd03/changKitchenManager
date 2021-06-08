db08a77ca7e0040cbded810a3a9c66025b5b1591
Dong Truong <dong23102003@gmail.com>
2021-06-08 19:03:01 +0700

    Organize files

---
+++ views/addDishPage/addDishPage.js
@@ -0,0 +1,17 @@
+import MenuDishes from '/support/js/class/MenuDishes.js';
+
+function load() {
+    for (i = 0; i < 10; ++i) {
+
+        //var tmp = new MenuDishes("main", "egg", 2, 20);
+        //console.log(tmp.getName());
+
+        var div = document.createElement('div');
+        div.className = "DishWrapper";
+        div.innerHTML = div.innerHTML + '<img src="https://res.cloudinary.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/ae3qqfev6j7hzhxw6if3" class="Image">';
+        div.innerHTML = div.innerHTML + '<p class="Name">Chocolate Ice-cream</p>';
+        div.innerHTML = div.innerHTML + '<p class="Price">$100</p>';
+
+        document.getElementById("Dishes").append(div);
+    }
+}
