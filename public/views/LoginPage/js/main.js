0c2c32924582086207aea8499e6dcbda96cc7d56
Dong Truong <dong23102003@gmail.com>
2021-06-13 20:08:40 +0700

    Add login mechanism

--- public/views/LoginPage/js/main.js
+++ public/views/LoginPage/js/main.js
@@ -12,87 +12,56 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-(function ($) {
-    "use strict";
-
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit',function(){
-        var check = true;
-
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
-
-        //if (check == true) {
-           
-        //}
-
-        if (check == true) {
-            window.alert("Congrats!")
-        }
-        return check;
-    });
-
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
-})(jQuery);
-
-function signIn() {
-    var check = true;
-
-    console.log("yeal")
+function logIn() {
     var email = $("#email").val();
     var password = $("#password").val();
+    
+    firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+                
+        console.log("YEAH")
+        window.location.replace("../menuPage/menuPage.html");
+        window.alert("Welcome back, Chang!");
 
-    var result = firebase.auth().signInWithEmailAndPassword(email, password);
-    result.catch(function(error) {
-        var errorCode = error.code;
+    }).catch(function(error) {
+
         var errorMessage = error.message;
-        
-        check = false
+        console.log(errorMessage);
         window.alert(errorMessage);
+
+        if (!errorMessage.includes("restore")) {
+
+            if (errorMessage.includes("email")) {
+                var input = document.getElementById("email");
+                showValidate(input);
+            } else {
+                var input = document.getElementById("password");
+                showValidate(input);
+            }
+        }
     });
 
-    if (check) {
-        window.alert("Congrats!");
+};
+
+$('.validate-form .input100').each(function(){
+    $(this).focus(function(){
+        hideValidate(this);
+    });
+});
+
+function validate (input) {
+    if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {        
+            return false;
+        }
     }
+}
+
+function showValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).addClass('alert-validate');
+}
+
+function hideValidate(input) {
+    var thisAlert = $(input).parent();
+    $(thisAlert).removeClass('alert-validate');
 }
