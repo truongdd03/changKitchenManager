3e063b44c3a2c1ba71f7181c533dbe69722c11fe
Dong Truong <dong23102003@gmail.com>
2021-06-15 22:40:08 +0700

    Add logout mechanism

--- public/views/support/js/topNavBar.js
+++ public/views/support/js/topNavBar.js
@@ -1,5 +1,27 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBRbiFdYbI13Ar2lOru5-HqMFSkf3rHDKM",
+    authDomain: "eshop-358c5.firebaseapp.com",
+    databaseURL: "https://eshop-358c5-default-rtdb.firebaseio.com",
+    projectId: "eshop-358c5",
+    storageBucket: "eshop-358c5.appspot.com",
+    messagingSenderId: "844303338405",
+    appId: "1:844303338405:web:0eb9dbf5cc44913bd8b64c",
+    measurementId: "G-M47CJ32FGP"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 function clickedAddDish() {
-    document.getElementById("AddDish").style.backgroundColor = "rgb(236, 179, 72)";
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log("Login!");
+            document.getElementById("AddDish").style.backgroundColor = "rgb(236, 179, 72)";
+        } else {
+            window.alert("Please login first!");
+            window.location = "/public/indexPage/indexPage.html";
+        }
+      });
 }
 
 function clickedMenu() {
@@ -15,5 +37,5 @@ function clickedUsers() {
 }
 
 function logOut() {
-    window.location = "/public/Authentication/LoginPage/LoginPage.html";
+    window.location = "/Authentication/LoginPage/LoginPage.html";
 }
