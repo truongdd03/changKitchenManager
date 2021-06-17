1d308fce3f01ae4fbcfac9a211af0e28e9e0a5d4
Dong Truong <dong23102003@gmail.com>
2021-06-17 11:28:55 +0700

    Change delete icon

--- public/views/usersPage/usersPage.js
+++ public/views/usersPage/usersPage.js
@@ -3,7 +3,7 @@ function load() {
         var div = document.createElement('div');
         div.innerHTML += '<p class="Name">Truong Dinh Dong</p>';
         div.innerHTML += '<p class="Phone">0911099706</p>';
-        div.innerHTML += '<img class="BinImage" src="../support/images/bin.jpg">'
+        div.innerHTML += '<i class="fa fa-trash"></i>'
         div.innerHTML += '<p class="Rank">Loyal User</p>';
         div.className = "UserWrapper";
 
