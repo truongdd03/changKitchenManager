daf626401cf7249bbf0c0304bb072766bc4494ec
Dong Truong <dong23102003@gmail.com>
2021-06-18 20:54:20 +0700

    Fetch users' data

--- public/views/usersPage/usersPage.js
+++ public/views/usersPage/usersPage.js
@@ -1,14 +1,35 @@
-function load() {
-    for (i = 0; i < 20; ++i) {
+var ref = firebase.database().ref();
+
+var listOfUsers = []; 
+
+function fetchData(callback) {
+    ref.child('users').on('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            var data = childSnapshot.val();
+            var name = data['firstname'] + " " + data['lastname'];
+            var phone = data['phone'];
+            var order = data['order'];
+            listOfUsers.push(new User(name, phone, order));
+        })
+        callback();
+    }).catch((error) => {
+        console.log(error.message);
+    });
+}
+
+function loadUsers() {
+    for (user of listOfUsers) {
         var div = document.createElement('div');
-        div.innerHTML += '<p class="Name">Truong Dinh Dong</p>';
-        div.innerHTML += '<p class="Phone">0911099706</p>';
+        div.innerHTML += '<p class="Name">' + user.name + '</p>';
+        div.innerHTML += '<p class="Phone">' + user.phone + '</p>';
         div.innerHTML += '<i class="fa fa-trash"></i>'
-        div.innerHTML += '<p class="Rank">Loyal User</p>';
+        div.innerHTML += '<p class="Rank">'+ user.rank + '</p>';
         div.className = "UserWrapper";
 
         document.getElementById('UsersWrapper').append(div);
     }
+}
 
-    var tmp = new User("Dong", "113", "Loyal");
+function load() {
+    fetchData(loadUsers);
 }
