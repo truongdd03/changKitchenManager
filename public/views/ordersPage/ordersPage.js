d3589eba8469bd997b2065b40dadfac8286ea64e
Dong Truong <dong23102003@gmail.com>
2021-06-19 18:25:25 +0700

    Fix bugs

--- public/views/ordersPage/ordersPage.js
+++ public/views/ordersPage/ordersPage.js
@@ -4,8 +4,6 @@ var currentID = -1;
 var listOfOrders = [];
 
 function fetchOrders(callback) {
-    console.log("YES");
-
     ref.child('orderStatus').on('value', function(snapshot) {
         snapshot.forEach(function(order) {
             var detail = order.val();
@@ -18,8 +16,9 @@ function fetchOrders(callback) {
 
             order.forEach(function(dish) {
                 var data = dish.val();
-                console.log(data);
-                orderDishes.push(new OrderDish(data['id'], data['note'], data['quantity']));
+                if (data['id'] != undefined) {
+                    orderDishes.push(new OrderDish(data['id'], data['note'], data['quantity']));
+                }
             })
 
             listOfOrders.push(new Order(id, pickUpTime, status, cost, uid, orderDishes));
@@ -56,15 +55,49 @@ function load() {
     fetchOrders(loadOrders);
 }
 
-function clicked(id) {
-    var div = document.createElement('div');
-    div.innerHTML = '<p> Clicked <\p>';
-
+function changeColorOfRow(id) {
     document.getElementById(id).style.backgroundColor = "rgb(245, 228, 200)";
     if (currentID != -1) {
         document.getElementById(currentID).style.backgroundColor = "rgb(245, 244, 219)";
     }
-
     currentID = id;
-    document.getElementById("DetailWrapper").append(div);
+}
+
+function findPositionOfOrder(id) {
+    for (i in listOfOrders) {
+        if (listOfOrders[i].id == id) {
+            return i;
+        }
+    }
+}
+
+function clicked(id) {
+    changeColorOfRow(id);
+
+    /*var limiter = document.createElement('div');
+    limiter.className = 'limiter';
+    
+    var container_table100 = document.createElement('div');
+    container_table100.className = "container-table100";
+
+    var wrap_table100 = document.createElement('div');
+    wrap_table100.className = "wrap_table100";*/
+
+
+    var table = document.createElement('div');
+    table.className = "table100";
+
+    var pos = findPositionOfOrder(id);
+    for (dish of listOfOrders[pos].dishes) {
+        var tr = document.createElement('tr');
+
+        var th1 = document.createElement('th');
+        th1.className = "column3";
+        th1.innerHTML = 
+        table.innerHTML += '<tr><th class="column3">' + dish.id + '</th>'
+        table.innerHTML += '<th class="column4">$' + 100 + '</th>'
+        table.innerHTML += '<th class="column5">' + dish.quantity + '</th>'
+        table.innerHTML += '<th class="column6">$' + 100 + '</th></tr>'
+    }
+
 }
